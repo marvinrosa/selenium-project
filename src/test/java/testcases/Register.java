1135e2cbe6cd44238cb80d7d8e4a2414ed1dca27
Marvin Rosa <marvin.fortin@outlook.com>
2023-01-21 18:50:50 -0600

    update register file with TestNG

--- src/test/java/testcases/Register.java
+++ src/test/java/testcases/Register.java
@@ -6,20 +6,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class Register {
 
-	public static void main(String[] args) {
-		
-		String EMAIL = "marvin.rosafortin@gmail.com";
-		String PASSWORD = "Owasp@1234";
-		String SELECT_ELEMENT = "\" Your favorite book? \"";
-		String SECURITY_ANSWER ="HarryPotter";
-		WebDriver driver = new ChromeDriver();
-		
+	String EMAIL = "marvin.rosafortin@gmail.com";
+	String PASSWORD = "Owasp@1234";
+	String SELECT_ELEMENT = "\" Your favorite book? \"";
+	String SECURITY_ANSWER ="HarryPotter";
+	WebDriver driver ;
+	
+	@BeforeMethod
+	public void setup(){
+		driver = new ChromeDriver();	
 		driver.get("https://juice-shop.herokuapp.com/#/register");
+		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		
+	}
+	
+	
+	@Test
+	public void register() {
 		WebElement email = driver.findElement(By.xpath("//*[@id=\"emailControl\"]"));
 		email.sendKeys(EMAIL);
 		
@@ -33,12 +42,20 @@ public class Register {
 		//Those elements is not working #1
 		WebElement selectQuestion = driver.findElement(By.xpath("//*[@id=\"mat-select-value-1\"]"));
 		selectQuestion.sendKeys(SELECT_ELEMENT);
+		
 		//#2
 		WebElement answerQuestion = driver.findElement(By.xpath("//*[@id=\"securityAnswerControl\"]"));
 		answerQuestion.sendKeys(SECURITY_ANSWER);
+		
 		//#3 not working
 		WebElement registerButton = driver.findElement(By.xpath("//*[@id=\"registerButton\"]/span[1]"));
 		registerButton.click();
+	}
+	
+	@AfterMethod()
+	public void tearDown(){
+		driver.close();
+		driver.quit();
 	}
 
 }
