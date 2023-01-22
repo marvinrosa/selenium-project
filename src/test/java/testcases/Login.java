44f7f38a2aadd2f4b4662e0ea202db1402bd95ad
Marvin Rosa <marvin.fortin@outlook.com>
2023-01-21 18:44:33 -0600

    update login file with TestNG

--- src/test/java/testcases/Login.java
+++ src/test/java/testcases/Login.java
@@ -1,23 +1,32 @@
 package testcases;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.locators.RelativeLocator;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class Login {
 
-	public static void main(String[] args) {
+	String EMAIL = "marvin.rosafortin@gmail.com";
+	String PASSWORD = "Owasp@1234";
+	String URL_LOGIN = "https://juice-shop.herokuapp.com/#/login";
+	WebDriver driver;
 	
-		String EMAIL = "marvin.rosafortin@gmail.com";
-		String PASSWORD = "Owasp@1234";
-		
-		WebDriver driver = new ChromeDriver();
-				
-		driver.get("https://juice-shop.herokuapp.com/#/login");
-		
-		
+	@BeforeMethod
+	public void setup(){
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get(URL_LOGIN);
+	}
+	
+	@Test
+	public void login() {
 		WebElement txtEmail = driver.findElement(By.xpath("//*[@id=\"email\"]"));
 		txtEmail.sendKeys(EMAIL);
 		
@@ -29,7 +38,12 @@ public class Login {
 		
 		WebElement lblError = driver.findElement(By.xpath("/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/app-login/div/mat-card/div[1]"));
 		System.out.print(lblError.equals("Invalid email or password."));
-		
 	}
-
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
+		driver.quit();
+	}
+	
 }
