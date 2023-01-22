d571910447fc2c8a950353ee8a860c005e140add
Marvin Rosa <marvin.fortin@outlook.com>
2023-01-21 19:41:10 -0600

    refactor using POM on LoginTest and LoginPage

---
+++ src/test/java/dev/store/pages/LoginPage.java
@@ -0,0 +1,39 @@
+package dev.store.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage {
+	
+	//Constantes
+	String LOGIN_URL = "https://juice-shop.herokuapp.com/#/login";
+
+	//Declaraciones
+	WebDriver driver;
+	
+	By txtEmail = By.name("email");
+	By txtPassword = By.id("password");
+	By btnLogin = By.xpath("//*[@id=\"loginButton\"]");
+	By lblError = By.id("loginButton");
+	//txtEmail.sendKeys(EMAIL);
+	//txtPassword.sendKeys(PASSWORD);
+	//btnLogin.click();
+	//System.out.print(lblError.equals("Invalid email or password."));
+	
+	//Constructor
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
+	}
+		
+	//Metodos
+	public void getUrl() {
+		this.driver.get(LOGIN_URL);
+	}
+	
+	public void fillOutForm(String username, String password) {
+		driver.findElement(txtEmail).sendKeys(username);
+		driver.findElement(txtPassword).sendKeys(password);
+		driver.findElement(btnLogin).submit();
+	}
+}
--- src/test/java/dev/store/testcases/LoginTest.java
+++ src/test/java/dev/store/testcases/LoginTest.java
@@ -1,32 +1,25 @@
 package dev.store.testcases;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import dev.store.pages.LoginPage;
 import dev.store.utils.BaseTest;
 
 public class LoginTest extends BaseTest{
 
+	//Contantes
 	String EMAIL = "marvin.rosafortin@gmail.com";
 	String PASSWORD = "Owasp@1234";
-	String URL_LOGIN = "https://juice-shop.herokuapp.com/#/login";
-	WebDriver driver;
 	
+	//Instancias
+	LoginPage login;
+	
+	//Testcases
 	@Test
 	public void login() {
-		WebElement txtEmail = driver.findElement(By.xpath("//*[@id=\"email\"]"));
-		txtEmail.sendKeys(EMAIL);
-		
-		WebElement txtPassword = driver.findElement(By.xpath("//*[@id=\"password\"]"));
-		txtPassword.sendKeys(PASSWORD);
-		
-		WebElement btnLogin = driver.findElement(By.xpath("//*[@id=\"loginButton\"]"));
-		btnLogin.click();
-		
-		WebElement lblError = driver.findElement(By.xpath("/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/app-login/div/mat-card/div[1]"));
-		System.out.print(lblError.equals("Invalid email or password."));
+		login = new LoginPage(driver);
+		login.getUrl();
+		login.fillOutForm(EMAIL, PASSWORD);	
 	}
-	
+
 }
