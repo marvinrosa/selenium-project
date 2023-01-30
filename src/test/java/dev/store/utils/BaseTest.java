180aa5af95fc551b7be2458518b6eade21271ebd
Marvin Rosa <marvin.fortin@outlook.com>
2023-01-30 16:53:02 -0600

    update baseTest config

--- src/test/java/dev/store/utils/BaseTest.java
+++ src/test/java/dev/store/utils/BaseTest.java
@@ -4,21 +4,37 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import dev.store.pages.HomePage;
+import dev.store.pages.LoginPage;
 
 public class BaseTest {
 
 	protected WebDriver driver;
+	protected HomePage homePage;
+	protected LoginPage loginPage;
 	
 	@BeforeMethod
 	public void setup() {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("http://localhost:3000");
+		
+		//
+		homePage = new HomePage(driver);
+		loginPage = new LoginPage(driver);
 	}
 	
 	@AfterMethod
+	public void afterMethod() {
+		//TODO	Screenshot
+	}
+	
+	@AfterClass
 	public void tearDown() {
 		//TODO	
 	}
